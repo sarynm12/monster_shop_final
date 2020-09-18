5e34a09267b4863b88ac0d21db8da97b9305a470
Saryn Mooney <smooney15@my.whitworth.edu>
2020-09-18 16:49:45 -0600

    created discount spec with passing relationships and validations

---
+++ spec/models/discount_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Discount do
+  describe 'relationships' do
+    it {should belong_to :merchant}
+  end
+
+  describe 'validations' do
+    it {should validate_presence_of :discount_percentage}
+    it {should validate_presence_of :minimum_quantity}
+  end
+end
