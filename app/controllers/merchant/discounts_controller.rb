c590a354d0b32578f9abbd5c81f4ff6561765595
Saryn Mooney <smooney15@my.whitworth.edu>
2020-09-19 09:48:33 -0600

    added create action to discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -6,6 +6,25 @@ class Merchant::DiscountsController < Merchant::BaseController
 
   def new
 
-  end 
+  end
+
+  def create
+    merchant = current_user.merchant
+    discount = merchant.discounts.new(discount_params)
+    if discount.save
+      redirect_to '/merchant/discounts'
+      flash[:success] = "Your new discount has been added"
+    else
+      flash[:notice] = "Please fill out all 3 fields"
+      redirect_to '/merchant/discounts/new'
+    end
+  end
+
+
+
+  private
+  def discount_params
+    params.permit(:discount_percentage, :minimum_quantity, :description)
+  end
 
 end
